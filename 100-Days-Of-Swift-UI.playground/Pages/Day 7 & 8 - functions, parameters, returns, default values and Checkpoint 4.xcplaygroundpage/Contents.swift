ab0e141bb5b9338042e6ddc1ba22f73635b50c26
smichaelonline <stephaniemichael001@gmail.com>
2023-03-21 21:53:13 -0400

    Finish Day 8 - get started on checkpoint 4.

--- 100-Days-Of-Swift-UI.playground/Pages/Day 7 & 8 - functions, parameters, returns, default values and Checkpoint 4.xcplaygroundpage/Contents.swift
+++ 100-Days-Of-Swift-UI.playground/Pages/Day 7 & 8 - functions, parameters, returns, default values and Checkpoint 4.xcplaygroundpage/Contents.swift
@@ -152,5 +152,90 @@ func printTimeTable(for number: Int){
 
 // As a performance optimization, Swift gives arrays just enough memory to hold their items plus a little extra so they can grow a little over time. If more items are added to the array, Swift allocates more and more memory automatically, so that as little as possible is wasted.
 
+// removeAll() - removes all items in the array and frees up the memory that was in the array.
+// keepingCapacity: - Boolean. removes the items while also keeping previous capacity. Has a default value of false.
+var characters = ["Lana", "Pam", "Ray", "Sterling"]
+characters.removeAll(keepingCapacity: true)
 
+//----------------------
+//HANDLING ERRORS IN FUNCTIONS
+/*
+ Swift needs us to handle errors:
+ 
+ Telling Swift about the possible errors that can happen.
+ Writing a function that can flag up errors if they happen.
+ Calling that function, and handling any errors that might happen.
+ */
 
+// EXAMPLE: if a user asks us to check how strong their password is. Flag an error if their password is too short or obvious.
+
+enum PasswordError: Error {
+  // defines two possible errors: short and obvious
+  case short, obvious
+}
+
+// if a function is able to throw an error without handling them itself - mark the function as throws before return
+// being marked throws doesn't mean the function must throw an error, just that is might.
+func checkPassword(_ password: String) throws -> String {
+  if password.count < 5 {
+    throw PasswordError.short
+  }
+  
+  if password == "12345" {
+    throw PasswordError.obvious
+  }
+  
+  if password.count < 8 {
+    return "OK"
+  } else if password.count < 10 {
+    return "Good"
+  } else {
+    return "Excellent"
+  }
+}
+
+// three steps to handling errors:
+/*
+ Starting a block of work that might throw errors, using do.
+ Calling one or more throwing functions, using try.
+ Handling any thrown errors using catch.
+ */
+
+/* psuedocode:
+ do {
+  try someRiskyWork()
+ } catch {
+  print("Handle errors here")
+ }
+ */
+
+let string = "12345"
+
+do {
+  let result = try checkPassword(string)
+  print("Password rating: \(result)")
+} catch PasswordError.short {
+  print("Plase use a longer password.")
+} catch PasswordError.obvious {
+  print("I have the same combination on my luggage")
+} catch {
+  print("There was an error.")
+}
+
+// if the checkPassword function works as planned, it will return a value into result.
+// if any arrors are thrown, the password rating will never be printed and execution will go into catch block.
+//try: must be written before calling all functinos that might throw errors.
+// you must be in a do block to use try. make sure to have one or more catch blocks to handle errors.
+// when it comes to catching errors, you have to have a catch block that can handle any kind of error.
+
+// Functions can throw errors: you create an enum defining the errors you want to happen, throw those errors inside the function as needed, then use do, try, and catch to handle them at the call site.
+
+//----------------------
+// CHECKPOINT 4
+// write a function that accepts an integer from 1 through 10,000, and returns the integer square root of that number. That sounds easy, but there are some catches:
+/*
+1. You can’t use Swift’s built-in sqrt() function or similar – you need to find the square root yourself.
+2. If the number is less than 1 or greater than 10,000 you should throw an “out of bounds” error.
+3. You should only consider integer square roots – don’t worry about the square root of 3 being 1.732, for example.
+4. If you can’t find the square root, throw a “no root” error.
+*/
